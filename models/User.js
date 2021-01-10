3f295d8a036e449ff90376af3dd6f480c518b6b9
ByunghoYoo <66407407+ByunghoYoo@users.noreply.github.com>
2021-01-10 16:05:26 +0900

    로그인, 로그아웃, Auth 기능 추가

--- models/User.js
+++ models/User.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcrypt')
 // salt를 이용해서 비밀번호를 암호화 해야하기 때문에 salt를 생성하기 위해 필요한 saltRounds 변수 선언
 const saltRounds = 10
+const jwt = require('jsonwebtoken')
 
 const userSchema = mongoose.Schema({
     name: {
@@ -51,8 +52,46 @@ userSchema.pre('save', function( next ){
                 next()
             })
         })
+    } else {
+        next()
     }
 })
+
+userSchema.methods.comparePassword = function(plainPassword, callback) {
+    // plainPassword(암호화 전)
+    bcrypt.compare(plainPassword, this.password, function(err, isMatch) {
+        if(err) return callback(err)
+        callback(null, isMatch)
+    })
+}
+
+userSchema.methods.generateToken = function(callback) {
+    var user = this
+
+    // jsonwebtoken을 이용해서 토큰 생성하기
+    var token = jwt.sign(user._id.toHexString(), 'secretToken')
+    // user._id + 'secretToken' = token
+    // -> 'secretToken' -> user._id
+    user.token = token
+    user.save(function(err, user) {
+        if(err) return callback(err)
+        callback(null, user)
+    })
+}
+
+userSchema.statics.findByToken = function(token, callback) {
+    var user = this
+
+    // 토큰 decode
+    jwt.verify(token, 'secretToken', function(err, decoded) {
+        // 유저 아이디를 이용해서 유저를 찾은 다음 
+        // 클라이언트에서 가져온 token과 DB에 보관된 토큰이 일치하는지 확인
+        user.findOne({ "_id": decoded, "token": token }, function(err, user) {
+            if(err) return callback(err);
+            callback(null, user)
+        })
+    })
+}
 
 const User = mongoose.model('User', userSchema)
 
