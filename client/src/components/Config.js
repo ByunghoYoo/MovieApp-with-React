65061d17c8afcae7223497bea4c6bb3712b3f955
ByunghoYoo <66407407+ByunghoYoo@users.noreply.github.com>
2021-01-19 22:40:26 +0900

    first commit

---
+++ client/src/components/Config.js
@@ -0,0 +1,8 @@
+//SERVER ROUTES
+export const USER_SERVER = '/api/users'
+
+export const API_URL = 'https://api.themoviedb.org/3/'
+
+export const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/'
+
+export const API_KEY = 'b8cbc507c2369fb8735891c47b1036a8'
---
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from 'react'
+import { FaCode } from "react-icons/fa";
+import { API_URL, API_KEY, IMAGE_BASE_URL } from '../../Config'
+import MainImage from './Sections/MainImage'
+
+function LandingPage() {
+
+    const [Movies, setMovies] = useState([])
+    const [MainMovieImage, setMainMovieImage] = useState(null)
+
+    useEffect(() => {
+        const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1` 
+
+        fetch(endpoint)
+        .then(response => response.json())
+        .then(response => {
+
+            setMovies([response.results])
+            setMainMovieImage(response.results[0])
+        })
+
+    }, [])
+
+    return (
+        <div style={{ width: '100%', margin: '0' }}>
+
+            {/* Main Image */}
+            {/* 이미지를 api로 불러오기전에 렌더링을 진행하기 때문에 &&를 넣어주어야 함 */}
+            {MainMovieImage &&
+                <MainImage 
+                image={`${IMAGE_BASE_URL}w1280${MainMovieImage.backdrop_path}`}
+                title={MainMovieImage.original_title}
+                text={MainMovieImage.overview}
+                />
+            }
+            <div style={{ width: '85%', margin: '1rem auto' }}>
+
+                <h2>Movies by latest</h2>
+                <hr/>
+
+                {/* Movie Grid Cards */}
+
+            </div>
+
+            <div style={{ display: 'flex', justifyContent: 'center' }}>
+                <button>Load More</button>
+            </div>
+        </div>
+    )
+}
+
+export default LandingPage
